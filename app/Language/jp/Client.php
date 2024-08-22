37cc0ea7d0a113455dfb74bea75c8b5f43ef925f
proxima <arcrise@naver.com>
2024-08-22 23:17:20 +0900

    Apply(feature) : Japanese Language

---
+++ app/Language/jp/Client.php
@@ -0,0 +1,138 @@
+<?php
+return [
+
+    //main
+    "menu_purchase_list" => "注文履歴",
+    "menu_project_list" => "プロジェクトリスト",
+    "menu_user_guide" => "利用ガイド",
+    "menu_inquiry" => "問い合わせ",
+    "show_detail" => "詳細を見る",
+
+    "relation" => "BECLE REACTION",
+    "popular_project" => "人気のあるプロジェクト",
+    "previous_project" => "前回のプロジェクト",
+
+    //main-footer
+    "ceo_name" => "代表",
+    "cs_center" => "カスタマーサポート",
+    "company_number" => "事業者登録番号",
+    "certification_number" => "通信販売業申告番号",
+    "manager_name" => "個人情報保護責任者",
+    "address" => "住所",
+
+    "main_info_title" => "思い出のはじまり",
+    "main_info_sub_title" => "私のファンに思い出をプレゼントしてください！",
+    "main_info_title_01" => "好きなアーティストを探してみてください！",
+    "main_info_title_02" => "好きなアーティストを探す",
+    "main_info_content_02" => "リクエストメッセージでお誕生日のお祝いメッセージや普段気になっていた内容を聞いてください！",
+    "main_info_title_03" => "アーティストにお便りを送ってください",
+    "main_info_content_03" => "アーティストが私の名前を呼び、私に必要な応援やアドバイス、私の事情に合ったオーダーメード映像を伝えてくれます。 家族や友達の一日をもっと完璧にしてくれる特別なお便りでもいいですよ.",
+    "main_info_title_04" => "アーティストに1:1ビデオメッセージをリクエストする",
+    "main_info_content_04" => "リクエストされたメッセージでアーティストが撮影すると、メールで動画を受け取ることができます！\nアーティストの映像準備時間が最大14日かかります。",
+    "main_info_title_05" => "映像を共有する",
+    "main_info_content_05" => "共有の際にはたくさんのイベントが用意されているので、友達やファンと一緒に映像を共有してみてください.",
+
+    "guide_title" => "BECLEサービスのご利用方法",
+
+    //reward
+    "reward_step_01" => "購入する数量を選択してください",
+    "reward_step_02" => "リクエストメッセージを作成してみてください",
+    "reward_step_03" => "決済して要請を完了してみてください",
+
+    "reward_purchase_item_01" => "どのようなタイプの映像をご希望ですか？",
+    "reward_purchase_item_02" => "このメッセージの対象は誰ですか？",
+    "reward_purchase_item_02_1" => "私のための映像です！",
+    "reward_purchase_item_02_2" => "他の人のための映像です！",
+    "reward_purchase_item_03" => "映像を受け取る人は誰ですか？",
+    "reward_purchase_item_03_1" => "受取人の名前またはニックネーム",
+    "reward_purchase_item_03_2" => "受信者のEメール",
+    "reward_purchase_item_04" => "リクエストメッセージを作成してください！",
+    "reward_purchase_item_05" => "映像非公開 (詳細ページに露出されません)",
+
+    "reward_type_all" => "メンバー全員",
+    "reward_type_random" => "ランダム",
+
+    "reward_now_stock_string" => "現在在庫%s",
+    "reward_limited_count_string" => "%s 個 限定",
+    "reward_available_count_string" => "%s 個 購入 可能",
+
+    "reward_reaction" => "分類",
+    "reward_request" => "要請事項",
+    "show_more" => "もっと見る",
+
+    "status_waiting" => "映像制作中",
+    "status_finished" => "映像制作完了",
+    "status_received" => "映像をもう一度受け取る",
+    "status_confirmed" => "映像を受けること",
+    "status_expired" => "映像満了",
+
+    "purchased_item_list" => "購入履歴",
+    "purchased_item" => "購入した商品",
+    "purchaser_name" => "購入者の名前",
+    "purchaser_email" => "購入者のEメール",
+    "purchaser_info" => "注文者情報",
+    "purchaser_notice" => "注文者情報で決済に関する情報が提供されます。 正確な情報として入力されていることを確認してください.",
+    "purchase_selected_item" => "選択した商品",
+
+    "purchase_no_item_message" => "購入履歴はありません。",
+    "purchase_complete_message" => "購入が完了しました.",
+    "purchase_complete_next" => "購入履歴から確認する",
+    "project_blocked_message" => "そのプロジェクトは非公開になりました.",
+    "project_blocked_next" => "メインに戻る",
+    "purchase" => "購入する",
+
+    "reward_select"  => "リワード選択",
+    "payment_method_select"  => "決済手段の選択",
+    "payment_method_nice"  => "NICE PAY",
+    "payment_method_inicis"  => "KG inicis",
+    "payment_expected_price"  => "決済予定金額",
+    "payment_agreement_string"  => "%sに同意します",
+    "payment_agreement"  => "注文情報およびサービス利用約款",
+    "payment_notice_01"  => "決済中に決済ウィンドウを閉じたり、ブラウザを終了したりする場合、決済が正常に完了しないことがあります。<br/>海外発行カードでお支払いの際は、言語設定を外国語に変更してお支払いを進めてください.",
+    "payment_notice_02"  => "すべての金額はウォンで決済され、為替レートによって注文金額と決済金額に差が生じることがあります。",
+    "payment_proceed"  => "決済する",
+
+    "email"  => "E-mail",
+    "project"  => "プロジェクト",
+    "reward"  => "リワード",
+    "available_count" => "購入可能な数量",
+
+    "datetime" => "一時",
+    "prev" => "以前",
+    "next" => "つぎ",
+    "refunded" => "返金処理済み",
+
+    "accepted" => "回答完了",
+    "requested" => "返事待ち",
+
+    // footer
+    "guide_qna" => "ガイドとよく聞かれる質問",
+    "agreement_service" => "サービス利用約款",
+    "agreement_personal" => "個人情報処理方針",
+    "show_information" => "案内表示",
+
+    "company_info_name" => 'koods Co.,LTD',
+    "company_info_ceo_name" => 'Song Eunhyuk',
+    "company_info_cs_center" => '010-3304-5446(平日 10:00~19:00)',
+    "company_info_company_number" => '208-86-02907',
+    "company_info_certification_number" => '제 2024-서울서대문-0699호',
+    "company_info_address" => 'Room 301, 3rd floor, 11-3, 1-dong, Yonsei-ro 2-da-gil, Seodaemun-gu, Seoul',
+    "company_info_email" => 'becle.official@gmail.com',
+
+    "terms_01" => "ビークルで販売されるコンテンツに対するすべての責任は株式会社クーズにあります.",
+    "terms_02" => "koods Co.,LTDサイトの商品/アーティスト/仲介サービス/取引情報、コンテンツ、UIなどに対する無断複製、伝送、配布、スクレイピングなどの行為は著作権法、コンテンツ産業振興法など関連法令により厳しく禁止されます.",
+
+    "message_error_field_empty" => "入力欄をすべて作成してください.",
+    "message_error_login" => "ログインが必要なサービスです.",
+    "message_error_exceed" => "購入可能数量を超過しました.",
+    "message_error_expired" => "プロジェクトが満了しました.",
+    "message_error_not_started" => "プロジェクトはまだ始まっていません.",
+
+    "auto_login" => "自動ログイン",
+    "auto_login_kakao" => "KAKAOTALK",
+    "auto_login_naver" => "NAVER",
+    "auto_login_google" => "GOOGLE",
+    "auto_login_register" => "登録する",
+    "auto_login_message_linked" => "アカウントはすでに連動しています.",
+];
+?>
